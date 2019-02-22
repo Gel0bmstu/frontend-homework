c645781b4fe86f7c0a93952391871b55c146ee18
Gel0bmstu <43792740+Gel0bmstu@users.noreply.github.com>
2019-02-22 23:18:01 +0300

    Added input validation, arrow functions and new tests; changed code style

--- source/minmax.js
+++ source/minmax.js
@@ -1,25 +1,33 @@
 'use strict';
 
-function minmax(input_str) {
+let minmax = (input_str) => {
+
+	// Валидация входных дынных (если не строка -> null)
+	if ( typeof(input_str) !== "string") {
+		return null;
+	}	
+
 	// Сплитим входную строку по пробелу
-	var splited_str = input_str.split(" "), 
-		parsed_arr = [];
+	let splited_str = input_str.split(" "); 
+	let parsed_arr = [];
 
 	// Пушим в конец инициализированного массива числа != NaN
-	splited_str.forEach(function(item, i, splited_str) {
-		if (isNaN(parseFloat(item))) {				 // Если элемент массива NaN
-			item = item.replace(/[^\d|/.|/-]/g, ""); // Убираем из него все символы, отличные от чисел, с помощью регулярного выражения (божьей)
-			if (item.length != 0)
-				parsed_arr.push(parseFloat(item));	 // Если длина получившегося выражения != 0, пушим его в массив чисел 
+	splited_str.forEach((item, i, splited_str) => {
+		if ( isNaN( parseFloat(item) ) ) {				 // Если элемент массива NaN
+			item = item.replace(/[^\d|/.|/-]/g, ""); 	 // Убираем из него все символы, отличные от чисел, с помощью регулярного выражения (божьей)
+			if (item.length !== 0) {
+				parsed_arr.push( parseFloat(item) );	 // Если длина получившегося выражения != 0, пушим его в массив чисел 
+			}
+		} else {
+		 parsed_arr.push( parseFloat(item) );			
 		}
-		else parsed_arr.push(parseFloat(item));			
 	})
 
-	// Иницилизируем min и max перемнные
-	var min, max;	
+	// Иницилизируем min и max перемнные	
+	let min, max;	
 
 	// Если массив не пустой -> ищем min & max элементы
-	if (parsed_arr.length != 0) {
+	if (parsed_arr.length !== 0) {
 		max = Math.max(...parsed_arr);
 		min = Math.min(...parsed_arr);
 	}
